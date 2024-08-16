374244c80eede532a171e40ac67b53d86ecc4fdd
Mohammed Atif Khan <saskhan1089@gmail.com>
2024-08-16 22:58:27 +0530

    Refactored auction server

--- backend/AuctionServer.js
+++ backend/AuctionServer.js
@@ -4,156 +4,93 @@ const app = express();
 const http = require('http');
 const AuctionServer = http.createServer(app);
 
-const {Server} = require('socket.io');
-
+const { Server } = require('socket.io');
 const AuctionModel = require('./models/Auction');
 const RoomModel = require('./models/AuctionRoom');
 
-const MAX_SOCKETS_PER_TAB = 2;
-const socketsPerTab = {};
-
-// const io cannnot emit to all sockets in room so make  it global by var 
-const io = new Server(AuctionServer,{
-    cors :{
-        origin : "*",
-        methods : ["GET","POST"],
-        allowedHeaders : ["my-custom-header"],
-        credentials : true
-    }
+const io = new Server(AuctionServer, {
+  cors: {
+    origin: "*",
+    methods: ["GET", "POST"],
+    allowedHeaders: ["my-custom-header"],
+    credentials: true
+  }
 });
 
-io.on('connection',(socket)=>{
+const auctionTimers = {};
 
-    const { id: socketId } = socket;
-  const { tabId } = socket.handshake.query;
-
-    socketsPerTab[tabId] = socketsPerTab[tabId] || 0;
-  socketsPerTab[tabId]++;
+io.on('connection', (socket) => {
   console.log("A user is Connected", socket.id);
-
-
-  // if (socketsPerTab[tabId] > MAX_SOCKETS_PER_TAB) {
-  //       socket.disconnect();
-  //       return;
-  //     }
-    
-      // // Otherwise, add the socket to the room
-      // socket.join('roomName');
-      
-      // When the socket disconnects, decrement the number of sockets for the tab
-      socket.on('disconnect', () => {
-        socketsPerTab[tabId]--;
-      });
-
-  socket.on("join room",async(data)=>{
-    console.log("Room code is ",data);
-    let resp = await RoomModel.findOne({Code : data.Code});
-    console.log(resp);
-    if(resp){
+  socket.on("join room", async (data) => {
+    console.log("Room code is ", data.Code);
+    let room = await RoomModel.findOne({ Code: data.Code });
+    if (room) {
       console.log("Joined Successfully");
       socket.join(data.Code);
-      socket.emit("startDetails",resp);
+      socket.emit("startDetails", room);
+      const endTime = new Date(room.endDate).getTime();
+      const currentTime = new Date().getTime();
 
-      //Find first bid by admin and send as starting bid
-      let respon = await AuctionModel.findOne({Room :data.Code});
-      if(respon)
-      {
-        socket.emit("starting_bid",respon.Bid);
+      if (currentTime >= endTime) {
+        socket.emit("auction_ended");
       }
-      else{
-        socket.emit("starting_bid",0);
+      else {
+        const timeLeft = endTime - currentTime;
+        if (auctionTimers[data.Code]) {
+          clearTimeout(auctionTimers[data.Code]);
+        }
+        auctionTimers[data.Code] = this.setTimeout(() => {
+          io.to(data.Code).emit("auction_ended");
+          endAuction(data.Code);
+        }, timeLeft);
       }
-      //latest bid is Current Highest bid
-      let res = await AuctionModel.find({Room:data.Code}).sort({_id:-1}).limit(1);
-      if(res)
-     {
-      console.log(res[0],"This is what we are sending");
-      socket.emit("curr_bid",res[0]);
-    //  res[0].Bid
-    } 
-     else{
-        socket.emit("curr_bid",0);
+
+      let startingBid = await AuctionModel.findOne({ Room: data.Code });
+      socket.emit("starting_bid", startingBid ? startingBid.Bid : 0);
+
+      let latestBid = await AuctionModel.findOne({ Room: data.Code }).sort({ _id: -1 });
+      if (latestBid) {
+        socket.emit("curr_bid", latestBid);
       }
-      //provide latest three bids
-
-      let response = await AuctionModel.find({Room : data.Code}).sort({_id:-1}).limit(3);
-      response.reverse();
-      socket.emit("bids",[...response]);
+      let recentBids = await AuctionModel.find({ Room: data.Code }).sort({ _id: -1 }).limit(3);
+      socket.emit("bids", recentBids.reverse());
+    } else {
+      socket.emit("room_error", data.Code);
     }
-    else{
-      socket.emit("room_error",data.Code);
-      // await RoomModel.create({Name :"first",Code : data.Code});
-      console.log("Enter valid Room code");
+  });
+  socket.on('send_bid', async (data) => {
+    if (!data.user) {
+      socket.emit("auth_error", { msg: "User does not exist!" });
+      return;
     }
-    // io.to(data.Code).emit("receive_bid",{bid:"-30",Code : data.Code,Name:"patel"});
-    // io.sockets.in(data.Code).emit("receive_bid",{bid:"-1",Code : data.Code,Name:"patel"});
-    // socket.to(data.Code).emit("receive_bid",{bid:"-2",Code : data.Code,Name:"patel"});
-  })
 
 
-  socket.on('send_bid',async(data)=>{
-    console.log("Data on Bid eve ",data);
-    if(!data.user){
-      socket.emit("auth_error",{msg:"user not exists!"});
+    let room = await RoomModel.findOne({ Code: data.Code });
+    if (!room || new Date() >= new Date(room.endDate)) {
+      socket.emit("error_bid", { message: "Auction has ended or does not exist" });
+      return;
     }
-    else{
-    let resp = await AuctionModel.find({Room:data.Code}).sort({_id:-1}).limit(1);
-    let res;
-    if(resp){
-      res = resp[0];
+
+    let latestBid = await AuctionModel.findOne({ Room: data.Code }).sort({ _id: -1 });
+    if (!latestBid || latestBid.Bid < data.bid) {
+      let newBid = await AuctionModel.create({ Bid: data.bid, User: data.User, Room: data.Code });
+      io.to(data.Code).emit('receive_bid', newBid);
+      io.to(data.Code).emit("curr_bid", newBid);
     }
-    // console.log(res.Bid,data.bid);
-    if((res && res.Bid < data.bid) || (!res)){
-      console.log(resp.Bid,data.bid);
-        bid = data.bid;
-        // socket.broadcast.to(data.Code).emit("receive_bid",data);
-         io.sockets.in(data.Code).emit('receive_bid', {Bid:data.bid,User : data.user,Room : data.Code});
-         io.sockets.in(data.Code).emit("curr_bid",{Bid:data.bid,User : data.user,Room : data.Code});
-         await AuctionModel.create({Bid:data.bid,User : data.user,Room : data.Code});
-        // io.to(data.Code).emit('receive_bid',data);
-        // socket.to(data.Code).emit("receive_bid",data);
+    else {
+      socket.emit("error_bid", { message: "Bid must be higher than the current highest bid" });
     }
-    else{
-      console.log("Error",data.bid,resp.Bid);
-      socket.emit("error_bid",res);
-    }
+  });
+
+  socket.on('disconnect', () => {
+    console.log("User disconnected", socket.id);
+  });
+});
+function endAuction(roomCode) {
+  if (auctionTimers[roomCode]) {
+    clearTimeout(auctionTimers[roomCode]);
+    delete auctionTimers[roomCode];
   }
-  })
-})
-
+}
 
 module.exports = AuctionServer;
-
-
-
-// To limit sockets per tab
-
-// Define a maximum limit of sockets per tab
-// const MAX_SOCKETS_PER_TAB = 5;
-
-// // Keep track of the number of sockets per tab
-// const socketsPerTab = {};
-
-// // When a socket connects, check if the tab has reached the limit
-// io.on('connection', (socket) => {
-//   const { id: socketId } = socket;
-//   const { tabId } = socket.handshake.query;
-
-//   // Increment the number of sockets for the tab
-//   socketsPerTab[tabId] = socketsPerTab[tabId] || 0;
-//   socketsPerTab[tabId]++;
-
-//   // If the limit has been reached, disconnect the socket
-//   if (socketsPerTab[tabId] > MAX_SOCKETS_PER_TAB) {
-//     socket.disconnect();
-//     return;
-//   }
-
-//   // Otherwise, add the socket to the room
-//   socket.join('roomName');
-  
-//   // When the socket disconnects, decrement the number of sockets for the tab
-//   socket.on('disconnect', () => {
-//     socketsPerTab[tabId]--;
-//   });
-// });
--- frontEnd/src/Components/Auction/Auction.js
+++ frontEnd/src/Components/Auction/Auction.js
@@ -1,122 +1,37 @@
-// import React, { useState, useEffect } from 'react';
-// import socketIOClient from 'socket.io-client';
-
-// const ENDPOINT = 'http://localhost:5000';
-
-// function Auction() {
-//   const [auctions, setAuctions] = useState([]);
-//   const [newBid, setNewBid] = useState({ itemId: '', bid: 0 });
-//   const [newComment, setNewComment] = useState({ itemId: '', username: '', comment: '' });
-
-//   useEffect(() => {
-//     const socket = socketIOClient(ENDPOINT);
-//     socket.on('auctions', (data) => {
-//       setAuctions(data);
-//     });
-//     return () => socket.disconnect();
-//   }, []);
-
-//   const handleBidChange = (event) => {
-//     setNewBid({ ...newBid, [event.target.name]: event.target.value });
-//   };
-
-//   const handleCommentChange = (event) => {
-//     setNewComment({ ...newComment, [event.target.name]: event.target.value });
-//   };
-
-//   const handleBidSubmit = (event) => {
-//     event.preventDefault();
-//     const socket = socketIOClient(ENDPOINT);
-//     socket.emit('newBid', newBid);
-//     setNewBid({ itemId: '', bid: 0 });
-//   };
-
-//   const handleCommentSubmit = (event) => {
-//     event.preventDefault();
-//     const socket = socketIOClient(ENDPOINT);
-//     socket.emit('newComment', newComment);
-//     setNewComment({ itemId: '', username: '', comment: '' });
-//   };
-//   console.log(auctions);
-//   return (
-//     <div>
-//       {auctions.map((auction) => (
-//         <div key={auction._id}>
-//           <h2>{auction.name}</h2>
-//           <p>{auction.description}</p>
-//           <p>Current bid: {auction.currentBid}</p>
-//           <form onSubmit={handleBidSubmit}>
-//             <label htmlFor="bid">Bid:</label>
-//             <input type="number" id="bid" name="bid" value={newBid.bid} onChange={handleBidChange} />
-//             <button type="submit">Submit bid</button>
-//           </form>
-//           <form onSubmit={handleCommentSubmit}>
-//             <label htmlFor="comment">Comment:</label>
-//             <input type="text" id="comment" name="comment" value={newComment.comment} onChange={handleCommentChange} />
-//             <label htmlFor="username">Username:</label>
-//             <input type="text" id="username" name="username" value={newComment.username} onChange={handleCommentChange} />
-//             <button type="submit">Submit comment</button>
-//           </form>
-//           <hr />
-//           {auction.comments.map((comment, index) => (
-//             <div key={index}>
-//               <p>{comment.username}: {comment.comment}</p>
-//             </div>
-//           ))}
-//         </div>
-//       ))}
-//       <form onSubmit={handleBidSubmit}>
-//             <label htmlFor="bid">Bid:</label>
-//             <input type="number" id="bid" name="bid" value={newBid.bid} onChange={handleBidChange} />
-//             <button type="submit">Submit bid</button>
-//           </form>
-//     </div>
-//   );
-// }
-
-// export default Auction;
-
-import React, { useContext, useEffect, useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import io from 'socket.io-client';
 import AuctionList from './AuctionList';
-import { nanoid } from 'nanoid';
-import DeepContext from '../../context/DeepContext';
 import "./Chat.css"
-const socket = io('http://localhost:3001');
 
-function Auction(props) {
-  // const {user} = props;
-  // console.log("heorweoroew ", props);
-  // localStorage.setItem('username',username);
-  const [price, setprice] = useState(0);
+const ENDPOINT = 'http://localhost:8001';
+const socket = io(ENDPOINT, {
+  transports: ['websocket', 'polling'],
+});
+
+function Auction() {
+  const [price, setPrice] = useState(0);
   const [amount, setAmount] = useState('');
   const [Code, setCode] = useState('');
-  const [bids, setbids] = useState([]);
-  const [h, setH] = useState([]);
+  const [bids, setBids] = useState([]);
   const [Start, setStart] = useState(0);
   const [user, setUser] = useState('');
   const [Cuser, setCuser] = useState('');
   const [sDate, setSDate] = useState('');
   const [cDate, setCDate] = useState(new Date());
-  // const {user,setUser}=useContext(DeepContext);
   const [Hig, setHig] = useState(0);
+  const [isAuctionEnded, setIsAuctionEnded] = useState(false);
+
   const handleSubmit = (event) => {
     event.preventDefault();
-    // setUser("qq");
-
-    // console.log("hii i am msg",user);
-    // console.log(Hig,amount);
-    if (Hig < amount) {
-      setprice(amount);
-      // console.log(user);
-      socket.emit('send_bid', { bid: amount, Code, user });
-      socket.on("error_bid", (data) => {
-        // console.log("Hello from inside");
-        alert("Invalid bid for User");
-      })
+    if (isAuctionEnded) {
+      alert("The auction has ended. No more bids can be placed.");
+      return;
     }
-    else {
-      alert("Please Enter valid bid");
+    if (Hig < amount) {
+      setPrice(amount);
+      socket.emit('send_bid', { bid: amount, Code, user });
+    } else {
+      alert("Please enter a valid bid");
     }
     setAmount('');
   };
@@ -124,109 +39,116 @@ function Auction(props) {
   const SubmitCode = (event) => {
     event.preventDefault();
     socket.emit('join room', { Code: Code });
+  };
+
+  useEffect(() => {
+    const user1 = localStorage.getItem('user');
+    setUser(user1);
+
+    socket.on("auth_error", (data) => {
+      alert(data.msg);
+    });
+
     socket.on("room_error", (data) => {
-      alert("can not find room with this id");
-    })
+      alert("Cannot find room with this id");
+    });
+
     socket.on("starting_bid", (data) => {
-      // console.log("data is here",data);
       setStart(data);
-    })
+    });
+
     socket.on("startDetails", (data) => {
-      console.log("This is details", cDate.getTime(), new Date(data.endDate).getTime());
-
       setSDate(new Date(data.endDate).toLocaleString());
-
-    })
+    });
 
     socket.on("curr_bid", (data) => {
       setHig(data.Bid);
       setCuser(data.User);
-    })
+    });
+
     socket.on("bids", (data) => {
-      // console.log("kya mast mossam hai",[...data]);
-      setbids((bids) => {
-        if (bids && !bids.includes(data)) {
-          setbids([...bids, ...data]);
-        }
-      })
-    })
-
-
-  }
-  useEffect(() => {
-    const user1 = localStorage.getItem('user');
-    console.log(user1);
-    setUser(user1);
-    socket.on("auth_error", (data) => {
-      alert(data.msg);
-
-    })
+      setBids((prevBids) => {
+        const newBids = data.filter(bid => !prevBids.some(prevBid => prevBid._id === bid._id));
+        return [...prevBids, ...newBids];
+      });
+    });
 
     socket.on("receive_bid", (data) => {
-      console.log(data);
-      setH((h) => [...h, data]);
-      console.log(h);
-      setbids((bids) => {
-        if (bids && !bids.includes(data)) {
-          setbids([...bids, data]);
-        }
-      })
-      socket.on("curr_bid", (data) => {
-        // console.log(data,"mast chhe j chhe e");
-        setHig(data.Bid);
-        setCuser(data.User);
-      })
-
+      setBids((prevBids) => [...prevBids, data]);
+      setHig(data.Bid);
+      setCuser(data.User);
     });
-    setCDate(new Date());
-    // console.log(user);
 
-  }, [socket, Cuser, bids])
+    socket.on("auction_ended", () => {
+      setIsAuctionEnded(true);
+      alert("The auction has ended.");
+    });
+
+    const checkAuctionEnd = () => {
+      const endTime = new Date(sDate).getTime();
+      const currentTime = new Date().getTime();
+      if (currentTime >= endTime) {
+        setIsAuctionEnded(true);
+      }
+    };
+
+    const timer = setInterval(checkAuctionEnd, 1000);
+    const dateTimer = setInterval(() => setCDate(new Date()), 1000);
+
+    return () => {
+      clearInterval(timer);
+      clearInterval(dateTimer);
+      socket.off("receive_bid");
+      socket.off("auth_error");
+      socket.off("room_error");
+      socket.off("starting_bid");
+      socket.off("startDetails");
+      socket.off("curr_bid");
+      socket.off("bids");
+      socket.off("auction_ended");
+    };
+  }, [sDate]);
 
   return (
-    <>
-
-      <div className="container mx-2">
-        <h1>Enter Code</h1>
-        <form onSubmit={SubmitCode}>
-          <input className='AuctionField'
-            type="text"
-            value={Code}
-            onChange={(event) => setCode(event.target.value)}
-          />
-          <button type="submit">Submit</button>
-        </form>
-        <div className="row">
-          <div className="col-md-8 offset-md-2">
-            <h1 className="text-center">Auction</h1>
-            <h3 className="text-center">Intial Bid : {Start}
-              || Highest Bid : {Hig}</h3>
-            <h3 className="text-center">End Date : {sDate} || Curr Date : {cDate.toLocaleString()}</h3>
-            {(new Date(sDate).getTime() < new Date(cDate).getTime()) ?
-              <>{(Cuser === user) ? <h1 className='text-center'>You Win</h1> : <h1 className='text-center'>{Cuser} wins the Auction</h1>}</> :
-              <>
-                <div className="chat-container">
-                  <AuctionList bids={bids} user={user} />
-                </div>
-                <form onSubmit={handleSubmit}>
-                  <div className="chat-input">
-                    <div className="input-group">
-                      <input type="number" value={amount} onChange={(event) => setAmount(event.target.value)} className="form-control" placeholder="Type your message..." />
-                      <div className="input-group-append">
-                        <button type="submit" className="btn btn-primary">Send</button>
-                      </div>
+    <div className="container mx-2">
+      <h1>Enter Code</h1>
+      <form onSubmit={SubmitCode}>
+        <input className='AuctionField'
+          type="text"
+          value={Code}
+          onChange={(event) => setCode(event.target.value)}
+        />
+        <button type="submit">Submit</button>
+      </form>
+      <div className="row">
+        <div className="col-md-8 offset-md-2">
+          <h1 className="text-center">Auction</h1>
+          <h3 className="text-center">Intial Bid : {Start} || Highest Bid : {Hig}</h3>
+          <h3 className="text-center">End Date : {sDate} || Current Date : {cDate.toLocaleString()}</h3>
+          {isAuctionEnded ? (
+            <h1 className='text-center'>
+              {Cuser === user ? "You Win" : `${Cuser} wins the Auction`}
+            </h1>
+          ) : (
+            <>
+              <div className="chat-container">
+                <AuctionList bids={bids} user={user} />
+              </div>
+              <form onSubmit={handleSubmit}>
+                <div className="chat-input">
+                  <div className="input-group">
+                    <input type="number" value={amount} onChange={(event) => setAmount(event.target.value)} className="form-control" placeholder="Enter your bid..." />
+                    <div className="input-group-append">
+                      <button type="submit" className="btn btn-primary">Place Bid</button>
                     </div>
                   </div>
-                </form>
-              </>
-            }
-
-
-          </div>
+                </div>
+              </form>
+            </>
+          )}
         </div>
-
       </div>
-    </>
+    </div>
   );
 }
 
